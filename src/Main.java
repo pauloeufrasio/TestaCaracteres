e0b426c08bbe2a7d67437ab49f48af07ebcffa9d
paulo <henriquepaulo0606@gmail.com>
2023-03-22 13:14:05 -0300

    Testando caracteres do tipo char e String

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // variavel do tipo char guarda um unico caractere obs: ao declara variavel do tipo char usar aspas simples
+        char letra = 'a';
+        System.out.println(letra);
+
+        //  Variavel do tipo String Guarda quantos caracteres voce deseja obs: ao usar variavel do tipo String usar aspas duplas
+        String palavra = " alura cursos online de tecnologia ";
+        System.out.println(palavra);
+    }
+}
